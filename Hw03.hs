a87558ba3c7a749cc4ef0f7639df9a2f9c9c3312
ukaj808 <ukaj808@gmail.com>
2022-07-12 17:59:08 -0400

    working solution excercise 3

--- Hw03.hs
+++ Hw03.hs
@@ -31,15 +31,31 @@ countOccur [] _ = 0
 countOccur (x:xs) y = if y == x
                       then (countOccur xs y) + 1
                       else countOccur xs y
-mapStar :: Eq a => a -> a -> Char
-mapStar x y = if x == y then '*' else ' '
 
-histoTally :: [Integer] -> String
-histoTally [] = "          \n" 
---histoTally xs = zipWith (mapStar $ countOccur) [0..9] xs 
-histoTally xs = zipWith mapStar (map (countOccur xs) [0..9]) [0..9]
+mapStar :: Integer -> Row -> Char
+mapStar numOccurs row = if numOccurs >= row then '*' else ' '
 
-histogram :: [Integer] -> String
-histogram [] = []
-histogram xs = (fst tup) ++ "\n" ++ (snd tup)
-    where tup = unzip (zip (repeat '=') ['0'..'9'])
+type Row = Integer
+type HistogramInput = [Integer]
+
+rowMap :: HistogramInput -> Row -> String
+rowMap _ (-1)     = "0123456789"
+rowMap _ 0        = "==========\n"
+rowMap [] _       = "          "
+rowMap input row  =
+    mapStar (countOccur input 0) row :
+    mapStar (countOccur input 1) row :
+    mapStar (countOccur input 2) row :
+    mapStar (countOccur input 3) row :
+    mapStar (countOccur input 4) row :
+    mapStar (countOccur input 5) row :
+    mapStar (countOccur input 6) row :
+    mapStar (countOccur input 7) row :
+    mapStar (countOccur input 8) row :
+    mapStar (countOccur input 9) row :
+    ['\n']
+
+histogram :: [Integer] -> String 
+histogram input =
+    concat $ zipWith rowMap (repeat input) [maxOccur, maxOccur - 1..(-1)]
+    where maxOccur = maximum (map (countOccur input) [0..9]) 
