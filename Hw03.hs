7ed88d2d0bec629a53d08825059ea2dc8694f9fe
ukaj808 <ukaj808@gmail.com>
2022-07-12 12:26:51 -0400

    Excercise 3 in progress

--- Hw03.hs
+++ Hw03.hs
@@ -25,3 +25,21 @@ getAllTriples _ = []
 
 localMaxima :: [Integer] -> [Integer]
 localMaxima xs = snd' (unzip3 (filter hasMaxima (getAllTriples xs)))
+
+countOccur :: Eq a => [a] -> a -> Integer
+countOccur [] _ = 0
+countOccur (x:xs) y = if y == x
+                      then (countOccur xs y) + 1
+                      else countOccur xs y
+mapStar :: Eq a => a -> a -> Char
+mapStar x y = if x == y then '*' else ' '
+
+histoTally :: [Integer] -> String
+histoTally [] = "          \n" 
+--histoTally xs = zipWith (mapStar $ countOccur) [0..9] xs 
+histoTally xs = zipWith mapStar (map (countOccur xs) [0..9]) [0..9]
+
+histogram :: [Integer] -> String
+histogram [] = []
+histogram xs = (fst tup) ++ "\n" ++ (snd tup)
+    where tup = unzip (zip (repeat '=') ['0'..'9'])
