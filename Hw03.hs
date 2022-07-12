c08e2cb55a9084cbbc098e3982fb097f1fff22b4
ukaj808 <ukaj808@gmail.com>
2022-07-11 20:34:51 -0400

    hw3 excercise 2

--- Hw03.hs
+++ Hw03.hs
@@ -12,3 +12,16 @@ everyNth n xs
 skips :: [a] -> [[a]]
 skips [] = []
 skips xs = zipWith everyNth [1..length xs] $ repeat xs
+
+snd' :: (a,a,a) -> a
+snd' (x,y,z) = y
+
+hasMaxima :: Ord a => (a,a,a) -> Bool
+hasMaxima (x,y,z) = y > x && y > z
+
+getAllTriples :: [a] -> [(a,a,a)]
+getAllTriples (x:rest@(y:z:xs)) = (x,y,z):getAllTriples rest 
+getAllTriples _ = []
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima xs = snd' (unzip3 (filter hasMaxima (getAllTriples xs)))
