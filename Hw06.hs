41846e1590a9d15704b19678cb987dbd74edf383
ukaj808 <ukaj808@gmail.com>
2022-10-09 09:16:41 -0400

    progress on hw 6; struggling with custom implmentation of stream

--- Hw06.hs
+++ Hw06.hs
@@ -15,3 +15,32 @@ fibs2 = [f i | i <- [0 ..]]
       | i <= 0 = 0
       | i <= 1 = 1
       | otherwise = fibs2 !! (i - 1) + fibs2 !! (i - 2)
+
+data MyList a = NilList | ConsList a (MyList a)
+  deriving (Show)
+
+data Stream a = Cons a (Stream a)
+
+streamToList :: Stream a -> [a]
+streamToList (Cons x (xs)) = x : streamToList xs
+
+listToStream :: [a] -> Stream a
+listToStream (x:xs) = Cons x (listToStream xs)
+
+streamRepeat :: a -> Stream a
+streamRepeat x = Cons x (streamRepeat x)
+
+streamMap :: (a -> b) -> Stream a -> Stream b
+streamMap f (Cons x (xs)) = Cons (f x) (streamMap f xs)
+
+streamFromSeed :: (a -> a) -> a -> Stream a
+streamFromSeed f x = Cons y (streamFromSeed f y)
+  where
+    y = f x
+
+nats :: Stream Integer
+nats = listToStream [0..] 
+
+instance Show a => Show (Stream a) where
+  show (Cons x (xs)) = take 20 ("Cons x " ++ (show xs))
+
