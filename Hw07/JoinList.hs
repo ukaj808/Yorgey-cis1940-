7c893baca0866b09b4259a8f8381cd891e884f85
fisnik <fisnik.ukaj@justeattakeaway.com>
2022-11-09 19:34:30 +0100

    progress on dropj

--- Hw07/JoinList.hs
+++ Hw07/JoinList.hs
@@ -47,25 +47,41 @@ indexJ i (Append m jl1 jl2) =
 joinListFold :: b -> (b -> a -> b -> b) -> JoinList m a -> b
 joinListFold e _ Empty = e 
 joinListFold e f (Single m a) = undefined
-joinListFold e f (Append m jl1 jl2) = undefined 
+joinListFold e f (Append m jl1 jl2) = undefined
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
 dropJ i jl | i <= 0 = jl
            | i >= joinListSize jl = Empty
 dropJ i Empty = Empty
-dropJ i (Append m jl1 jl2) = undefined
-
-test =
+dropJ i (Append m jl1 jl2) | joinListSize jl1 == i = Append m Empty jl2
+                           | joinListSize jl1 > i = Append m (dropJ i jl1) jl2
+test1 =
   Append
-    (Size 7)
+    (Size 10)
     (Append
-       (Size 6)
+       (Size 8)
        (Append
-          (Size 2)
-          (Append (Size 1) Empty (Single (Size 1) 'e'))
-          (Append (Size 1) (Single (Size 1) 'a') Empty))
+          (Size 4)
+          (Append (Size 2) (Single (Size 1) 'y') (Single (Size 1) 'e'))
+          (Append (Size 2) (Single (Size 1) 'a') (Single (Size 1) 'h')))
        (Append
           (Size 4)
           (Append (Size 2) (Single (Size 1) ' ') (Single (Size 1) 'b'))
           (Append (Size 2) (Single (Size 1) 'u') (Single (Size 1) 'd'))))
-    (Append (Size 1) (Single (Size 1) 'd') Empty)
+    (Append (Size 2) (Single (Size 1) 'd') (Single (Size 1) 'y'))
+
+test2 =
+  Append
+    (Size 10)
+    (Append
+       (Size 8)
+       (Append
+          (Size 4)
+          (Append (Size 2) (Single (Size 1) 'i') (Single (Size 1) 't'))
+          (Append (Size 2) (Single (Size 1) 's') (Single (Size 1) ' ')))
+       (Append
+          (Size 4)
+          (Append (Size 2) (Single (Size 1) 't') (Single (Size 1) 'i'))
+          (Append (Size 2) (Single (Size 1) 'm') (Single (Size 1) 'e'))))
+    (Append (Size 2) (Single (Size 1) 'r') (Single (Size 1) 'y'))
+
