cde4f9913a1156712ae7c9b3253fd00df34b9ccd
fisnik <ukaj808@gmail.com>
2022-11-10 18:55:31 +0100

    takej complete

--- Hw07/JoinList.hs
+++ Hw07/JoinList.hs
@@ -33,35 +33,59 @@ indexJ i jl
   | i >= rootValue = Nothing
   where
     rootValue = getSize (size (tag jl))
-
 indexJ i (Single m a) =
   if i == 0
     then Just a
     else Nothing
-
 indexJ i (Append m jl1 jl2) =
   if i < joinListSize jl1
-    then case jl1 of Empty -> indexJ (i - joinListSize jl1) jl2
-                     _ -> indexJ i jl1
+    then case jl1 of
+           Empty -> indexJ (i - joinListSize jl1) jl2
+           _ -> indexJ i jl1
     else indexJ (i - joinListSize jl1) jl2
 
 joinListFold :: b -> (b -> a -> b -> b) -> JoinList m a -> b
-joinListFold e _ Empty = e 
+joinListFold e _ Empty = e
 joinListFold e f (Single m a) = undefined
 joinListFold e f (Append m jl1 jl2) = undefined
 
 dropJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
-dropJ i jl | i <= 0 = jl
-           | i >= joinListSize jl = Empty
-dropJ i Empty = Empty
-dropJ i (Append m jl1 jl2) |  i == jl1Size = Append (tag Empty `mappend` tag jl2) Empty jl2
-                           |  i > jl1Size = Append (tag Empty `mappend` jl2'm) Empty jl2'
-                           |  i < jl1Size = Append (jl1'm `mappend` jl2'm) jl1' jl2
-                                where jl1Size = joinListSize jl1
-                                      jl1' = dropJ i jl1
-                                      jl2' = dropJ (i - joinListSize jl1) jl2
-                                      jl1'm = tag jl1'
-                                      jl2'm = tag jl2'
+dropJ i jl
+  | i <= 0 = jl
+  | i >= joinListSize jl = Empty
+dropJ _ Empty = Empty
+dropJ i (Append m jl1 jl2)
+  | i == jl1Size = Append (tag Empty `mappend` tag jl2) Empty jl2
+  | i > jl1Size = Append (tag Empty `mappend` jl2'm) Empty jl2'
+  | i < jl1Size = Append (jl1'm `mappend` jl2'm) jl1' jl2
+  where
+    jl1Size = joinListSize jl1
+    jl1' = dropJ i jl1
+    jl2' = dropJ (i - joinListSize jl1) jl2
+    jl1'm = tag jl1'
+    jl2'm = tag jl2'
+
+takeJ :: (Sized b, Monoid b) => Int -> JoinList b a -> JoinList b a
+takeJ i jl
+  | i <= 0 = Empty
+  | i >= joinListSize jl = jl
+takeJ _ Empty = Empty
+takeJ i (Append m jl1 jl2)
+  | i == jl1Size = Append (tag jl1 `mappend` tag Empty) jl1 Empty
+  | i < jl1Size = Append (jl1'm `mappend` tag Empty) jl1' Empty
+  | i > jl1Size = Append (tag jl1 `mappend` jl2'm) jl1 jl2'
+  where
+    jl1Size = joinListSize jl1
+    jl1' = takeJ i jl1
+    jl1'm = tag jl1'
+    jl2' = takeJ (i - joinListSize jl1) jl2
+    jl2'm = tag jl2'
+
+jlToList :: JoinList m a -> [a]
+jlToList Empty = []
+jlToList (Single _ a) = [a]
+jlToList (Append _ l1 l2) = jlToList l1 ++ jlToList l2
+
 test1 =
   Append
     (Size 10)
@@ -91,4 +115,3 @@ test2 =
           (Append (Size 2) (Single (Size 1) 't') (Single (Size 1) 'i'))
           (Append (Size 2) (Single (Size 1) 'm') (Single (Size 1) 'e'))))
     (Append (Size 2) (Single (Size 1) 'r') (Single (Size 1) 'y'))
-
