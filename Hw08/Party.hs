7924546972fde9ab40d4508d482b684380de6b99
fisnik <fisnik.ukaj@justeattakeaway.com>
2022-11-17 21:15:33 +0100

    progress on ex 1

--- Hw08/Party.hs
+++ Hw08/Party.hs
@@ -3,5 +3,16 @@ module Party where
 import Employee (Employee, GuestList(GL), empFun)
 
 
+instance Semigroup GuestList where
+  (<>) (GL emps1 fun1) (GL emps2 fun2) = GL (emps1 ++ emps2) (fun1 + fun2)
+
+instance Monoid GuestList where
+  mempty = GL [] 0
+
 glCons :: Employee -> GuestList -> GuestList
 glCons emp (GL emps fun) = GL (emp : emps) (empFun emp + fun) 
+
+moreFun :: GuestList -> GuestList -> GuestList
+moreFun gl1@(GL _ fun1) gl2@(GL _ fun2) = if fun1 > fun2 then gl1 else gl2
+
+
