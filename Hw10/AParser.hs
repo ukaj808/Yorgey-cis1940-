e7d1b82ce596a8c98d701795e35673563763e4a2
fisnik <ukaj808@gmail.com>
2022-12-18 14:20:29 +0100

    formatted

--- Hw10/AParser.hs
+++ Hw10/AParser.hs
@@ -1,19 +1,22 @@
 {-# LANGUAGE InstanceSigs #-}
+
 {- CIS 194 HW 10
    due Monday, 1 April
 -}
-
 module AParser where
 
-import           Control.Applicative
+import Control.Applicative
 
-import           Data.Char
+import Data.Char
 
 -- A parser for a value of type a is a function which takes a String
 -- represnting the input to be parsed, and succeeds or fails; if it
 -- succeeds, it returns the parsed value along with the remainder of
 -- the input.
-newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
+newtype Parser a =
+  Parser
+    { runParser :: String -> Maybe (a, String)
+    }
 
 -- For example, 'satisfy' takes a predicate on Char, and constructs a
 -- parser which succeeds only if it sees a Char that satisfies the
@@ -22,12 +25,12 @@ newtype Parser a = Parser { runParser :: String -> Maybe (a, String) }
 satisfy :: (Char -> Bool) -> Parser Char
 satisfy p = Parser f
   where
-    f [] = Nothing    -- fail on the empty input
-    f (x:xs)          -- check if x satisfies the predicate
+    f [] = Nothing -- fail on the empty input
+    f (x:xs) -- check if x satisfies the predicate
                         -- if so, return x along with the remainder
                         -- of the input (that is, xs)
-        | p x       = Just (x, xs)
-        | otherwise = Nothing  -- otherwise, fail
+      | p x = Just (x, xs)
+      | otherwise = Nothing -- otherwise, fail
 
 -- Using satisfy, we can define the parser 'char c' which expects to
 -- see exactly the character c, and fails otherwise.
@@ -44,16 +47,16 @@ Nothing
 Just ('x',"yz")
 
 -}
-
 -- For convenience, we've also provided a parser for positive
 -- integers.
 posInt :: Parser Integer
 posInt = Parser f
   where
     f xs
-      | null ns   = Nothing
+      | null ns = Nothing
       | otherwise = Just (read ns, rest)
-      where (ns, rest) = span isDigit xs
+      where
+        (ns, rest) = span isDigit xs
 
 ------------------------------------------------------------
 -- Your code goes below here
@@ -63,5 +66,6 @@ first f (x, y) = (f x, y)
 
 instance Functor Parser where
   fmap :: (a -> b) -> Parser a -> Parser b
-  fmap f (Parser runParserF) = Parser runParserG 
-    where runParserG xs = fmap (first f) (runParserF xs)
+  fmap f (Parser runParserF) = Parser runParserG
+    where
+      runParserG xs = fmap (first f) (runParserF xs)
